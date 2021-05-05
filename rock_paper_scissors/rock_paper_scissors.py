2d4b4a2712c11528cd4cf743f96aa8daf6f2d4d8
navneet <navneetsidhube@gmail.com>
2021-05-05 21:04:22 +0530

    changes

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -2,16 +2,21 @@ import random
 
 
 def play():
-    user = input("Enter you choice:'r' for rock,'p' for 'paper','s' for scissor ")
+    user = input("Enter your choice:'r' for rock,'p' for 'paper','s' for scissor: ")
     computer = random.choice(['r','p','s'])
     # r > s, s > p , p > r
+    
     if user == computer:
-        return 'Its a tie'
+        return 'Its a tie -_-'
     if is_win(user,computer):
-        return 'You Won'
+        return 'You Won :)'
 
-    return 'You lost'
+    return 'You lost :('
 
- def is_win(player,opponent):
-     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
-         return True          
+def is_win(player,opponent):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+        return True 
+ #   else:
+ #       print("Enter a valid choice and by entering wrong choice")
+
+print(play())         
