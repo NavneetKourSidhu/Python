97b7df277b8c622edbde9404b5d1c5d5bebc0b2b
navneet <navneetsidhube@gmail.com>
2021-05-12 22:20:21 +0530

    one-liners

---
+++ one-liners/one_liners.py
@@ -0,0 +1,22 @@
+# swapping two variables #
+
+#a = 3
+#b = 8
+#print(a,b)
+#a,b = b,a
+#print(a,b)
+
+# Multiple variable assignments #
+
+#a,b,c = 1,2.2,'Snoop'
+#a,b,*c = [1,2,3,4,5,3,2]
+#print(a,b,c)
+
+# Sum of Even numbers in a list #
+
+#a = [1,4,8,10,2,6]
+#s = sum([num for num in a if num%2 == 0])
+#print(s)
+
+# Deleting multiple Elements from a list
+
