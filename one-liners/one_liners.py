4ec7eba008f56a5ab1ddef3023d2aeaa002d8d5c
navneet <navneetsidhube@gmail.com>
2021-05-15 23:30:09 +0530

    set,list

--- one-liners/one_liners.py
+++ one-liners/one_liners.py
@@ -20,3 +20,25 @@
 
 # Deleting multiple Elements from a list
 
+# Deleting aal even
+#a = [1,2,3,4,5]
+#del a[1::2]
+#print(a)
+
+# Creating lists
+
+#list = [i for i in range(0,20)]
+#print(list)  
+
+        #OR
+#list = list(range(0,5))
+#print(list) 
+
+        # list of strings
+#list = [("Hello "+i) for i in ['Snoop',"Snoopy"]]
+#print(list)
+
+# Set creation
+
+print({x**2 for x in range(10) if x%2 ==0})
+
