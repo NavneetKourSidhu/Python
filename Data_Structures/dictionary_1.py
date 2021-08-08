ad58dceb808b4c3a2ed9d24df7d6047dc1a38db8
navneet <navneetsidhube@gmail.com>
2021-08-08 19:22:18 +0530

    dictionary functions

---
+++ Data_Structures/dictionary_1.py
@@ -0,0 +1,37 @@
+# empty dictionary
+dictionary1 = {}
+
+# dictionary with integer keys
+dictionary1 = {1: 'snoop', 2: 'jojo'}
+
+# dictionary with mixed keys
+dictionary1 = {'name': 'jojo', 1: [3, 4, 5]}
+
+# using dictionary()
+dictionary1 = dict({1: 'snoop' , 2: 'jojo'})
+
+# from sequence having each item as a pair
+dictionary1 = dict([(1, 'snoop'), (2, 'jojo')])
+
+#### Accessing elements from dictionary
+print(dictionary1[1])
+print(dictionary1.get(2))
+
+#### Trying to access keys which doesn't exist
+
+print(dictionary1.get(3))
+
+##### Key error
+#print(dictionary1[3])
+
+###### Changing and adding dictionary elements
+dictionary1 = {'name': 'snoop', 'age': 4}
+
+# update value
+dictionary1['age'] = 3
+
+print(dictionary1)
+
+## Add item 
+dictionary1['address'] = 'Sri Ganganagar'
+print(dictionary1)
